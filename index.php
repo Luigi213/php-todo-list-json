410ff8e99b4cebf8bf7d530d091ac762da1e8d78
Luigi213 <reyesgianluigi88@gmail.com>
2023-02-01 16:13:27 +0100

    styled

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.3.0/axios.min.js" integrity="sha512-A6BG70odTHAJYENyMrDN6Rq+Zezdk+dFiFFN6jH1sB+uJT3SYMV4zDSVR+7VawJzvq7/IrT/2K3YWVKRqOyN0Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="./css/style.css">
         <title>Document</title>
     </head>
@@ -22,8 +23,11 @@
                         <h1>Todo List</h1>
                     </div>
                     <div class="col-12 d-flex align-items-center flex-column w-25 bg-white rounded-1">
-                        <div class="text-black border-bottom w-100 p-4" v-for="list in toDoList">
-                            {{list.language}}
+                        <div class="d-flex text-decoration-line-through justify-content-between text-black border-bottom w-100 p-4" v-for="list in toDoList">
+                            {{list.language}}  
+                            <div>
+                                <button class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></button>
+                            </div> 
                         </div>
                     </div>
                     <div class="col-12 mt-4">
